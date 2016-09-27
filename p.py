eb7b0bf6c63a55b455370b8f20dd767d374080c5
Youlin Yi <youlin.yi@gmail.com>
2016-09-26 20:52:46 -0700

    test

--- p.py
+++ p.py
@@ -1,17 +1,3 @@
 def p(n):
-    """Return True if it is a prime n."""
-    for e in range(2,n):
-        if n % e == 0:
-	    print e
-            return False
     return True
-
-def next_p(n):
-    """find the closest prime n larger than *n*."""
-    i = n
-    while True:
-        i += 1
-        if p(i):
-            print(i)
-
 
